7c6db928a7af05af3125aa9f6a064a880cbd7de9
Zhe Li <linuxcity.jn@gmail.com>
2013-11-13 22:44:13 +0100

    added a test for post API

--- rest_demo/rest_demo/demo/tests.py
+++ rest_demo/rest_demo/demo/tests.py
@@ -6,11 +6,17 @@ Replace this with more appropriate tests for your application.
 """
 
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+class PostAPITests(APITestCase):
+    def test_create_post(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Test if new post can be created.
         """
-        self.assertEqual(1 + 1, 2)
+        url = reverse('post-list')
+        data = {'post':'this is a test'}
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(response.data, None)
